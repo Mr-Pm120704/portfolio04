0941bf25f438c23a6829cc0621d940845682a6ba
MuruganandhamP <muruganandhamm7639@gmail.com>
2024-06-10 17:29:10 +0530

    Your descriptive commit message

--- connection.php
+++ connection.php
@@ -1,24 +1,21 @@
 <?php
 
-$MYSQL_ADDON_HOST="bxz3ducvbt8zlq5eftua-mysql.services.clever-cloud.com";
-$MYSQL_ADDON_DB="bxz3ducvbt8zlq5eftua";
-$MYSQL_ADDON_USER="uckfjannptteicuc";
-$MYSQL_ADDON_PORT="3306";
-$MYSQL_ADDON_PASSWORD="Cj;4sJBsBzhuHLv5D0LeH";
-$MYSQL_ADDON_URI="mysql://uckfjannptteicuc:Cj4sJBsBzhuHLv5D0LeH@bxz3ducvbt8zlq5eftua-mysql.services.clever-cloud.com:3306/bxz3ducvbt8zlq5eftua";
+// Database connection details
+$MYSQL_ADDON_HOST = "bxz3ducvbt8zlq5eftua-mysql.services.clever-cloud.com";
+$MYSQL_ADDON_DB = "bxz3ducvbt8zlq5eftua";
+$MYSQL_ADDON_USER = "uckfjannptteicuc";
+$MYSQL_ADDON_PORT = "3306";
+$MYSQL_ADDON_PASSWORD = "Cj;4sJBsBzhuHLv5D0LeH";
 
- $host=getenv($MYSQL_ADDON_HOST);
- $user=getenv($MYSQL_ADDON_USER);
- $password=getenv($MYSQL_ADDON_PASSWORD);
- $dbname=getenv($MYSQL_ADDON_DB);
- $port=getenv($MYSQL_ADDON_PORT);
- $uri=getenv($MYSQL_ADDON_URI);
- $conn=mysqli_connect($host,$user,$password,$dbname,$port,$uri);
- if(mysqli_connect_errno())
- {
- echo("failedto connect db".mysqli_connect_error());
- }
- else 
-   echo("<form action='contect.php' method='POST'>");
+// Attempt to establish a connection to the database
+$conn = mysqli_connect($MYSQL_ADDON_HOST, $MYSQL_ADDON_USER, $MYSQL_ADDON_PASSWORD, $MYSQL_ADDON_DB, $MYSQL_ADDON_PORT);
 
+// Check connection
+if (mysqli_connect_errno()) {
+    echo "Failed to connect to the database: " . mysqli_connect_error();
+} else {
+    echo "<form action='contect.php' method='POST'>";
+    // Your form fields go here
+    echo "</form>";
+}
 ?>
